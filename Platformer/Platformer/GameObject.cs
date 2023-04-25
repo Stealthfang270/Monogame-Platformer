2c3709609382507c7f2c308349b6d259bd912b82
Matthew Whelan Keats <mattdragon03@gmail.com>
2023-04-25 15:30:06 -0600

    Added allowed objects

---
+++ Platformer/Platformer/GameObject.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PlatformerGame
+{
+    internal class GameObject : DrawableGameComponent
+    {
+        internal Transform _transform;
+        internal Rectangle _rectangleBounds;
+        internal Texture2D _texture;
+        internal Vector2 Position => _transform.Position;
+
+
+        // Each child should override/make a new spritebatch.
+        // Objects of the same class can share the spritebatch.
+        public static SpriteBatch spriteBatch;
+
+        public Transform Transform => _transform;
+        public Rectangle RectangleBounds => _rectangleBounds;
+        public Texture2D Texture => _texture;
+        public GameObject(Game game, Transform transform, Rectangle rectangle, Texture2D texture) : base(game)
+        {
+            if (spriteBatch is null)
+            {
+                spriteBatch = spriteBatch = new SpriteBatch(GraphicsDevice);
+            }
+
+            _transform = transform;
+            _rectangleBounds = rectangle;
+            _texture = texture;
+            game.Components.Add(this);
+        }
+        //public override void Initialize()
+        //{
+        //    base.Initialize();
+        //}
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            
+        }
+        public override void Draw(GameTime gameTime)
+        {
+            //base.Draw(gameTime);
+            spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+            spriteBatch.Draw(_texture, _transform.Position, _texture.Bounds, Color.White, _transform.Rotation, _texture.Bounds.Center.ToVector2(), _transform.Scale, SpriteEffects.None, 0);
+            spriteBatch.End();
+        }
+    }
+}
---
+++ Platformer/Transform.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using MonoGameExtensions;
+
+namespace PlatformerGame
+{
+    /// <summary>
+    /// Describes general data as a point in space, with rotation + scale.
+    /// </summary>
+    internal struct Transform
+    {
+        Vector2 _position;
+        float _rotation;
+        int _scale;
+
+        public Vector2 Position => _position;
+        public float Rotation => _rotation;
+        public int Scale => _scale;
+        public Point Location(Point referenceSize) => _position.Location(referenceSize);
+
+        public Transform(Vector2 position, float rotation, int scale)
+        {
+            _position = position;
+            _rotation = rotation;
+            _scale = scale;
+            
+        }
+
+        public void SetPosition(Vector2 newPosition)
+        {
+            _position = newPosition;
+        }
+        public void SetPosition(float x, float y)
+        {
+            _position = new Vector2(x, y);
+        }
+        public void SetPosition(Rectangle rect)
+        {
+            _position = rect.Center.ToVector2();
+        }
+
+        public Vector2 MovePosition(Vector2 posOffset)
+        {
+            SetPosition(_position + posOffset);
+            return _position + posOffset;
+        }
+
+    }
+
+}
+
